35834b6930baae43a4ac717c4cfa87d7047d8cb6
vadger <vadger@yahoo.com>
2021-04-07 01:25:15 +0300

    add button scroll to top

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const galleryThumbs = new Swiper('.pagination-slider', {
+document.addEventListener( 'DOMContentLoaded', function() {
+const scrollTop = document.getElementById('scroll-top');
+  scrollTop.addEventListener('click', function(  ){
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+  window.addEventListener("scroll", function(){
+    if(window.scrollY==0){
+      //user is at the top of the page; no need to show the back to top button
+      scrollTop.style.opacity = "0";
+    } else {
+      scrollTop.style.opacity = "1";
+    }
+  });
+
+  const galleryThumbs = new Swiper( '.pagination-slider', {
     spaceBetween: 10,
-    slidesPerView: 1,
+    slidesPerView: 3,
     freeMode: true,
     watchSlidesVisibility: true,
     watchSlidesProgress: true,
@@ -20,9 +36,9 @@ document.addEventListener('DOMContentLoaded', function () {
         spaceBetween: 20,
       },
     },
-  });
+  } );
 
-  const galleryTop = new Swiper('.main-slider', {
+  const galleryTop = new Swiper( '.main-slider', {
     // Navigation arrows
     spaceBetween: 10,
     slidesPerView: 1,
@@ -53,73 +69,72 @@ document.addEventListener('DOMContentLoaded', function () {
     thumbs: {
       swiper: galleryThumbs,
     },
-  });
-  document.querySelectorAll('.header__list-item a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+  } );
+  document.querySelectorAll( '.header__list-item a[href^="#"]' ).forEach( anchor => {
+    anchor.addEventListener( 'click', function( e ) {
       e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
+      document.querySelector( this.getAttribute( 'href' ) ).scrollIntoView( {
         behavior: 'smooth',
-      });
-    });
-  });
+      } );
+    } );
+  } );
   // ==================
   // === modal-menu ===
   // ==================
   (() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
-    const closeMenuItem = document.querySelectorAll('.js-close-popup');
+    const mobileMenu = document.querySelector( '.js-menu-container' );
+    const openMenuBtn = document.querySelector( '.js-open-menu' );
+    const closeMenuBtn = document.querySelector( '.js-close-menu' );
+    const closeMenuItem = document.querySelectorAll( '.js-close-popup' );
     const toggleMenu = () => {
-      const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-open');
-      closeMenuItem.forEach(item => {
-        item.addEventListener('click', function () {
-          mobileMenu.classList.remove('is-open');
-          openMenuBtn.setAttribute('aria-expanded', false);
-        });
-      });
+      const isMenuOpen = openMenuBtn.getAttribute( 'aria-expanded' ) === 'true' || false;
+      openMenuBtn.setAttribute( 'aria-expanded', !isMenuOpen );
+      mobileMenu.classList.toggle( 'is-open' );
+      closeMenuItem.forEach( item => {
+        item.addEventListener( 'click', function() {
+          mobileMenu.classList.remove( 'is-open' );
+          openMenuBtn.setAttribute( 'aria-expanded', false );
+        } );
+      } );
     };
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
+    document.querySelectorAll( 'a[href^="#"]' ).forEach( anchor => {
+      anchor.addEventListener( 'click', function( e ) {
         e.preventDefault();
-        console.log('click');
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
+        document.querySelector( this.getAttribute( 'href' ) ).scrollIntoView( {
           behavior: 'smooth',
-        });
-      });
-    });
+        } );
+      } );
+    } );
 
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
+    openMenuBtn.addEventListener( 'click', toggleMenu );
+    closeMenuBtn.addEventListener( 'click', toggleMenu );
 
     // Закрываем мобильное меню на более широких экранах
     // в случае изменения ориентации устройства.
-    window.matchMedia('(min-width: 1345px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
-    });
+    window.matchMedia( '(min-width: 1345px)' ).addEventListener( 'change', e => {
+      if ( !e.matches ) return;
+      mobileMenu.classList.remove( 'is-open' );
+      openMenuBtn.setAttribute( 'aria-expanded', false );
+    } );
   })();
-});
 // ==================
 //  === backdrop ====
 // ==================
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    openModalPopUp: document.querySelector('[data-modal-openPopUp]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    backdrop: document.querySelector('[data-backdrop]'),
-  };
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector( '[data-modal-open]' ),
+      openModalPopUp: document.querySelector( '[data-modal-openPopUp]' ),
+      closeModalBtn: document.querySelector( '[data-modal-close]' ),
+      backdrop: document.querySelector( '[data-backdrop]' ),
+    };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.openModalPopUp.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.openModalBtn.addEventListener( 'click', toggleModal );
+    refs.openModalPopUp.addEventListener( 'click', toggleModal );
+    refs.closeModalBtn.addEventListener( 'click', toggleModal );
 
-  function toggleModal() {
-    document.body.classList.toggle('modal-open');
-    refs.backdrop.classList.toggle('backdrop__hidden');
-  }
-})();
+    function toggleModal() {
+      document.body.classList.toggle( 'modal-open' );
+      refs.backdrop.classList.toggle( 'backdrop__hidden' );
+    }
+  })();
+} );
